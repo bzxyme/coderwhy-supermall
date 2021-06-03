c329858ba23615bda070f2a6e335595452773731
LeoEchoAurora <leoechoaurora@gmail.com>
2021-06-03 09:13:15 +0800

    首页nav

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,24 @@
 <!-- 组件说明 -->
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center><div>购物街</div></template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar.vue";
+
 //import x from ''
+// import NavBar from 'components/common/navbar/NavBar'
+
 export default {
   name: "home",
-  components: {},
+  components: {
+    NavBar
+    // NavBar
+  },
   data() {
     return {};
   },
@@ -16,4 +27,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color:#fff
+}
+</style>
