4c754e339f723f1d7fd711a1ac02c655b2a33d60
LeoEchoAurora <leoechoaurora@gmail.com>
2021-06-03 16:20:28 +0800

    首页推荐信息

---
+++ src/views/home/childComponents/RecommendView.vue
@@ -0,0 +1,59 @@
+<!-- 组件说明 -->
+<template>
+  <div class="recommends">
+    <div
+      v-for="(item, index) in recommends"
+      :key="index"
+      class="recommends-item"
+    >
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+        <div>
+          {{ item.title }}
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+//import x from ''
+export default {
+  name: "RecommendView",
+  components: {},
+  data() {
+    return {};
+  },
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style>
+.recommends {
+  display: flex;
+  text-align: center;
+  width: 100%;
+  font-size: 12px;
+
+  padding: 10px 0 30px;
+  border-bottom: 8px solid #eee;
+}
+.recommends-item {
+  flex: 1;
+}
+.recommends-item img {
+  /* width: 65px; */
+  width: 70%;
+  /* height: 65px; */
+  margin-bottom: 5px;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,13 +4,15 @@
     <nav-bar class="home-nav">
       <template v-slot:center><div>购物街</div></template>
     </nav-bar>
-    <home-swiper :banners="banners"/>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar.vue";
 import HomeSwiper from "./childComponents/HomeSwiper.vue";
+import RecommendView from "./childComponents/RecommendView.vue";
 
 // import Home from ''
 
@@ -23,7 +25,8 @@ export default {
   name: "home",
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView
     // NavBar
   },
   data() {
